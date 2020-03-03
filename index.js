1dea030e0ac0000fdb1d3b6a3cf54e43da0be801
p-b-3 <40724307+p-b-3@users.noreply.github.com>
2020-03-03 13:05:08 -0500

    confernces

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 app.get("/db", async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query("SELECT * FROM test_table");
+    const result = await client.query("SELECT * FROM conferences_test2");
     const results = { results: result ? result.rows : null };
     res.send(results);
     client.release();
