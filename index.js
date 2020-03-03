81751c8779c3a5fc7134898970bd97bfd7f9f821
p-b-3 <40724307+p-b-3@users.noreply.github.com>
2020-03-03 11:54:48 -0500

    db

--- index.js
+++ index.js
@@ -7,21 +7,23 @@ const pool = new Pool({
   ssl: true
 });
 
+
+
+app.get("/", (req, res) => {
+  res.send({ users: 1 });
+});
+
 app.get("/db", async (req, res) => {
   try {
     const client = await pool.connect();
     const result = await client.query("SELECT * FROM test_table");
     const results = { results: result ? result.rows : null };
-    res.render("pages/db", results);
+    res.send(results);
     client.release();
   } catch (err) {
     console.error(err);
     res.send("Error " + err);
   }
-});
-
-app.get("/", (req, res) => {
-  res.send({ users: 1 });
 });
 
 const PORT = process.env.PORT || 5000;
