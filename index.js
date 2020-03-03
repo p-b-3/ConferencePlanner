185d73741236c6140acfb683942bf3aa85caeeb1
p-b-3 <patrrb3@gmail.com>
2020-03-03 08:48:06 -0500

    data

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const { Pool } = require("pg");
+
+const app = express();
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send({ user: 1 });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
