890686ff59b70e8a20ecea375e69861fc0dc6cff
p-b-3 <40724307+p-b-3@users.noreply.github.com>
2020-03-04 14:47:22 -0500

    confernces

--- queries.js
+++ queries.js
@@ -14,6 +14,59 @@ const getConferences = (req, res) => {
   })
 }
 
+const getConferenceById = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('SELECT * FROM conferences_test3 WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+const createConference = (request, response) => {
+  const { name, email } = request.body
+
+  pool.query('INSERT INTO conferences_test3 (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(201).send(`Conference added with ID: ${result.insertId}`)
+  })
+}
+
+const updateConference = (request, response) => {
+  const id = parseInt(request.params.id)
+  const { name, email } = request.body
+
+  pool.query(
+    'UPDATE conferences_test3 SET name = $1, email = $2 WHERE id = $3',
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).send(`Conference modified with ID: ${id}`)
+    }
+  )
+}
+
+const deleteConferences = (request, response) => {
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM conferences_test3 WHERE id = $1', [id], (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).send(`Conference deleted with ID: ${id}`)
+  })
+}
+
 module.exports = {
   getConferences,
+  getConferenceById,
+  createConference,
+  updateConference,
+  deleteConference,
 }
