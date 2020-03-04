1a6a8efbbb0a95ed3ebea7882030037180b4673d
p-b-3 <40724307+p-b-3@users.noreply.github.com>
2020-03-04 13:34:19 -0500

    confernces

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const { Pool } = require("pg");
+const db = require('./queries')
 
 const app = express();
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});
+
 
 
 
@@ -14,18 +11,20 @@ app.get("/", (req, res) => {
   res.send({ users: 1 });
 });
 
-app.get("/db", async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * FROM conferences_test3");
-    const results = { results: result ? result.rows : null };
-    res.send(results);
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
+// app.get("/db", async (req, res) => {
+//   try {
+//     const client = await pool.connect();
+//     const result = await client.query("SELECT * FROM conferences_test3");
+//     const results = { results: result ? result.rows : null };
+//     res.send(results);
+//     client.release();
+//   } catch (err) {
+//     console.error(err);
+//     res.send("Error " + err);
+//   }
+// });
+
+app.get("api/conferences", dg.getConferences)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
---
+++ queries.js
@@ -0,0 +1,19 @@
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+
+const getConferences = (req, res) => {
+  pool.query('SELECT * FROM conferences_test3', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+module.exports = {
+  getConferences,
+}
