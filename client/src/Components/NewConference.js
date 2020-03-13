bf5bdeec2e5e0866cb53be3ac63e673ba8fc7f58
p-b-3 <40724307+p-b-3@users.noreply.github.com>
2020-03-13 17:57:54 -0400

    update

--- client/src/Components/NewConference.js
+++ client/src/Components/NewConference.js
@@ -100,7 +100,7 @@ class NewConference extends Component {
             <input
               type="number"
               value={this.state.attendance_goal}
-              name = "attendance_current"
+              name = "attendance_goal"
               onChange = {this.onInputChange}
             />
 
